09a1b3149b34722f49b67b00ecdf0894c1a83e5c
Eric Kidd <git@randomhacks.net>
2010-06-21 15:41:35 -0400

    spike: More GUIDs and header fields

--- spike.cpp
+++ spike.cpp
@@ -33,8 +33,17 @@ prop_id_name_info prop_name_map[] = {
     { 0x0037, L"PidTagSubject" },
     { 0x0039, L"PidTagClientSubmitTime" },
     { 0x003b, L"PidTagSentRepresentingSearchKey" },
+    { 0x003f, L"PidTagReceivedByEntryId" },
+    { 0x0040, L"PidTagReceivedByName" },
     { 0x0041, L"PidTagSentRepresentingEntryId" },
     { 0x0042, L"PidTagSentRepresentingName" },
+    { 0x0043, L"PidTagReceivedRepresentingEntryId" },
+    { 0x0044, L"PidTagReceivedRepresentingName" },
+    { 0x0047, L"PidTagMessageSubmissionId" },
+    { 0x0051, L"PidTagReceivedBySearchKey" },
+    { 0x0052, L"PidTagReceivedRepresentingSearchKey" },
+    { 0x0057, L"PidTagMessageToMe" },
+    { 0x0058, L"PidTagMessageCcMe" },
     { 0x0060, L"PidTagStartDate" },
     { 0x0061, L"PidTagEndDate" },
     { 0x0062, L"PidTagOwnerAppointmentId" },
@@ -43,6 +52,12 @@ prop_id_name_info prop_name_map[] = {
     { 0x0065, L"PidTagSentRepresentingEmailAddress" },
     { 0x0070, L"PidTagConversationTopic" },
     { 0x0071, L"PidTagConversationIndex" },
+    { 0x0075, L"PidTagReceivedByAddressType" },
+    { 0x0076, L"PidTagReceivedByEmailAddress" },
+    { 0x0077, L"PidTagReceivedRepresentingAddressType" },
+    { 0x0078, L"PidTagReceivedRepresentingEmailAddress" },
+    { 0x007d, L"PidTagTransportMessageHeaders" },
+    { 0x007f, L"PidTagTnefCorrelationKey" },
     { 0x0c17, L"PidTagReplyRequested" },
     { 0x0c19, L"PidTagSenderEntryId" },
     { 0x0c1a, L"PidTagSenderName" },
@@ -58,19 +73,30 @@ prop_id_name_info prop_name_map[] = {
     { 0x0e1f, L"PidTagRtfInSync" },
     { 0x0e23, L"PidTagInternetArticleNumber" },
     { 0x0e27, L"PidTagSecurityDescriptor" },
+    { 0x0e2b, L"PidTagToDoItemFlags" },
+    //{ 0x0e2f, L"" },
     { 0x0e79, L"PidTagTrustSender" },
     { 0x1000, L"PidTagBody" },
     { 0x1009, L"PidTagRtfCompressed" },
     { 0x1013, L"PidTagBodyHtml" },
     { 0x1035, L"PidTagInternetMessageId" },
     { 0x1080, L"PidTagIconIndex" },
+    { 0x1090, L"PidTagFlagStatus" },
+    // Prop 0x1095 allegedly "specifies the flag color of the Message object."
+    { 0x1095, L"PidTagFollowupIcon" },
     { 0x10f4, L"PidTagAttributeHidden" },
     { 0x10f5, L"PidTagAttributeSystem" },
     { 0x10f6, L"PidTagAttributeReadOnly" },
     { 0x3007, L"PidTagCreationTime" },
     { 0x3008, L"PidTagLastModificationTime" },
     { 0x300b, L"PidTagSearchKey" },
+    { 0x3010, L"PidTagTargetEntryId" },
+    //{ 0x3014, L"" },
+    //{ 0x3015, L"" },
+    { 0x3016, L"PidTagConversationIndexTracking" },
+    { 0x3a40, L"PidTagSendRichInfo" },
     { 0x3fde, L"PidTagInternetCodepage" },
+    { 0x3fea, L"PidTagHasDeferredActionMessages" },
     { 0x3ff1, L"PidTagMessageLocaleId" },
     { 0x3ff8, L"PidTagCreatorName" },
     { 0x3ff9, L"PidTagCreatorEntryId" },
@@ -79,18 +105,28 @@ prop_id_name_info prop_name_map[] = {
     { 0x3ffd, L"PidTagMessageCodepage" },
     { 0x4019, L"PidTagSenderFlags" },
     { 0x401a, L"PidTagSentRepresentingFlags" },
-    // I can't find these in the docs.
+    { 0x401b, L"PidTagReceivedByFlags" },
+    { 0x401c, L"PidTagReceivedRepresentingFlags" },
     //{ 0x405d, L"" },
+    { 0x5902, L"PidTagInternetMailOverrideFormat" },
+    { 0x5909, L"PidTagMessageEditorFormat" },
+    { 0x5d01, L"PidTagSenderSmtpAddress" },
+    //{ 0x5d02, L"" },
+    { 0x5fe5, L"PidTagSessionInitiationProtocolUri" },
     //{ 0x6540, L"" },
+    { 0x65e2, L"PidTagChangeKey" },
+    { 0x65e3, L"PidTagPredecessorChangeList" },
     //{ 0x6610, L"" },
+    //{ 0x6619, L"" },  // Not PidTagUserEntryId, I don't think.
     { 0, NULL }
 };
 
-const guid ps_common_headers = { 0x62008, 0, 0, { 0xc0, 0, 0, 0, 0, 0, 0, 0x46 } };
+const guid ps_common = { 0x62008, 0, 0, { 0xc0, 0, 0, 0, 0, 0, 0, 0x46 } };
 const guid ps_internet_headers = { 0x20386, 0, 0, { 0xc0, 0, 0, 0, 0, 0, 0, 0x46 } };
 const guid ps_task = { 0x62003, 0, 0, { 0xc0, 0, 0, 0, 0, 0, 0, 0x46 } };
 const guid ps_messaging = { 0x41f28f13, static_cast<ushort>(0x83f4), 0x4114, { 0xa5, 0x84, 0xee, 0xdb, 0x5a, 0x6b, 0x0b, 0xff } };
 const guid ps_appointment = { 0x62002, 0, 0, { 0xc0, 0, 0, 0, 0, 0, 0, 0x46 } };
+const guid ps_meeting = { 0x6ed8da90, 0x450b, 0x101b, { 0x98, 0xda, 0, 0xaa, 0, 0x3f, 0x13, 0x05 } };
 
 bool operator==(const guid g1, const guid g2) {
     return (g1.data1 == g2.data1 &&
@@ -106,8 +142,8 @@ wstring guid_name(guid g) {
         return L"ps_mapi";
     } else if (g == ps_public_strings) {
         return L"ps_public_strings";
-    } else if (g == ps_common_headers) {
-        return L"ps_common_headers";
+    } else if (g == ps_common) {
+        return L"ps_common";
     } else if (g == ps_internet_headers) {
         return L"ps_internet_headers";
     } else if (g == ps_task) {
@@ -116,6 +152,8 @@ wstring guid_name(guid g) {
         return L"ps_messaging";
     } else if (g == ps_appointment) {
         return L"ps_appointment";
+    } else if (g == ps_meeting) {
+        return L"ps_meeting";
     } else {
         wostringstream out;
         out << hex

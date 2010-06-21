c869eac0d71c49106cad3145b16631b8207b0d5f
Eric Kidd <git@randomhacks.net>
2010-06-21 10:39:04 -0400

    spike: Dump recipients and attachments

--- spike.cpp
+++ spike.cpp
@@ -18,8 +18,33 @@ R prop_or(const T &obj, R (T::*pmf)() const, D default_value) {
     }
 }
 
+void process_recipient(const recipient &r) {
+    wcout << "  ";
+    switch (prop_or(r, &recipient::get_type, recipient_type(0))) {
+        case mapi_to: wcout << "To"; break;
+        case mapi_cc: wcout << "CC"; break;
+        case mapi_bcc: wcout << "BCC"; break;
+        default: wcout << "(Unknown type)"; break;
+    }
+    wcout << ": " << prop_or(r, &recipient::get_name, L"(anonymous)") << " <"
+          << prop_or(r, &recipient::get_email_address, L"(no email)")
+          << ">" << endl;
+}
+
+void process_attachment(const attachment &a) {
+    wcout << "  Attachment: "
+          << prop_or(a, &attachment::get_filename, L"(no filename)") << " ("
+          << prop_or(a, &attachment::size, 31337) << " bytes)"
+          << endl;
+}
+
 void process_message(const message &m) {
-    wcout << prop_or(m, &message::get_subject, L"(No subject)") << endl;
+    wcout << prop_or(m, &message::get_subject, L"(null)") << endl;
+
+    if (m.get_recipient_count() > 0)
+        for_each(m.recipient_begin(), m.recipient_end(), process_recipient);
+    if (m.get_attachment_count() > 0)
+        for_each(m.attachment_begin(), m.attachment_end(), process_attachment);
 }
 
 void process_folder(const folder &f) {

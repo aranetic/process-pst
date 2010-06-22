ec08aa5e7c918a4387be71bbcf15397917210196
Eric Kidd <git@randomhacks.net>
2010-06-22 13:04:13 -0400

    Open a PST file for reading

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <pstsdk/pst.h>
+
+#include "utilities.h"
 
 using namespace std;
+using namespace pstsdk;
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
+    // Parse our command-line arguments.
+    if (argc != 3) {
         wcout << L"Usage: process-pst input.pst output-dir" << endl;
         exit(1);
     }
+    string pst_path(argv[1]);
+    string output_directory_path(argv[2]);
+
+    // Open our PST.
+    try {
+        pst pst_db(string_to_wstring(pst_path));
+    } catch (exception &e) {
+        wcerr << L"Could not open PST: " << string_to_wstring(e.what());
+        exit(1);
+    }
+
+    return 0;
 }

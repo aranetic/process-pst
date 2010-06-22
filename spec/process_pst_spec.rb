b5fb39df78b70dfb3f540e7a810c101b9853067c
Eric Kidd <git@randomhacks.net>
2010-06-22 16:10:46 -0400

    Store processing output in out/, and delete after each spec

--- spec/process_pst_spec.rb
+++ spec/process_pst_spec.rb
@@ -1,16 +1,25 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "process-pst" do
+  after do
+    FileUtils.rm_rf(build_path("out"))
+  end
+
   it "should fail if passed invalid arguments" do
     process_pst("nosuch.pst", "out").should == false
   end
 
-  it "should succeed if passed valid arguments" do
-    process_pst("pstsdk/test/sample1.pst", "sample1-out").should == true
-  end
+  context "sample1.pst" do
+    before do
+      @result = process_pst("pstsdk/test/sample1.pst", "out")
+    end
 
-  it "should create a directory and edrm loadfile" do
-    process_pst("pstsdk/test/sample1.pst", "sample1-out")
-    File.exist?(build_path("sample1-out/edrm-loadfile.xml")).should == true
+    it "should succeed if passed valid arguments" do
+      @result.should == true
+    end
+
+    it "should create a directory and edrm loadfile" do
+      File.exist?(build_path("out/edrm-loadfile.xml")).should == true
+    end
   end
 end

554eada91a05badd4cec6970afa3e9c11fbb61c6
Eric Kidd <git@randomhacks.net>
2010-06-26 09:53:56 -0400

    Factor out has_prop so we don't check for so many exceptions

--- document.cpp
+++ document.cpp
@@ -5,11 +5,23 @@ using namespace std;
 using boost::any;
 using namespace pstsdk;
 
-document::document(const pstsdk::message &m) {
-    try {
-        (*this)[L"#Subject"] = wstring(m.get_subject());
-    } catch (key_not_found<prop_id>&) {
+namespace {
+    // Rather than spend quite so much time checking for exceptions, let's
+    // build a simple wrapper function which checks for us.
+    template <typename R, typename T>
+    bool has_prop(const pstsdk::message &m, R (T::*pmf)() const) {
+        try {
+            (m.*pmf)();
+            return true;
+        } catch (key_not_found<prop_id> &) {
+            return false;
+        }
     }
+}
+
+document::document(const pstsdk::message &m) {
+    if (has_prop(m, &message::get_subject))
+        (*this)[L"#Subject"] = wstring(m.get_subject());
 }
 
 any &document::operator[](const wstring &key) {

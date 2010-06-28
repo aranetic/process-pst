0ad958fa54d237ccf6b462aa8c90e1aaebf09782
Eric Kidd <git@randomhacks.net>
2010-06-28 08:32:10 -0400

    Refactor XML writing into standalone class
    This will allow us to carry our ostream and indentation context
    implicitly.

--- edrm.cpp
+++ edrm.cpp
@@ -87,79 +87,83 @@ wstring edrm_tag_value(const any &value) {
     throw runtime_error("Unable to output EDRM TagValue for value");
 }
 
-namespace xml {
-    string xml_declaration() {
-        return "<?xml version='1.0' encoding='UTF-8'?>\n";
+class xml_context {
+    ostream &m_out;
+    int m_indent;
+
+    void indent() {
+        for (int i = 0; i < m_indent; ++i)
+            m_out << "  ";
     }
 
-    string indent(size_t levels) {
-        string result;
-        for (size_t i = 0; i < levels; ++i)
-            result += "  ";
-        return result;
+public:
+    xml_context(ostream &out) : m_out(out), m_indent(0) {
+        m_out << "<?xml version='1.0' encoding='UTF-8'?>" << endl;
     }
 
-    string tag_open(size_t levels, const string &tag_name) {
-        return indent(levels) + "<" + tag_name;
+    xml_context &lt(const string &tag_name) {
+        indent();
+        m_out << "<" << tag_name;
+        return *this;
     }
 
-    string attr(const string &name, const wstring &value) {
-        return " " + name + "='" + xml_quote(value) + "'";
+    xml_context &attr(const string &name, const wstring &value) {
+        m_out << " " << name << "='" << xml_quote(value) << "'";
+        return *this;
     }
 
-    string tag_close(size_t &levels) {
-        ++levels;
-        return ">\n";
+    void gt() {
+        m_out << ">" << endl;
+        ++m_indent;
     }
 
-    string tag_close_empty(size_t &levels) {
-        return "/>\n";
+    void slash_gt() {
+        m_out << "/>" << endl;
     }
 
-    string start_tag(size_t &levels, const string &tag_name) {
-        return tag_open(levels, tag_name) + tag_close(levels);
+    void start_tag(const string &tag_name) {
+        lt(tag_name).gt();
     }
 
-    string end_tag(size_t &levels, const string &tag_name) {
-        return indent(levels--) + "</" + tag_name + ">\n";
+    void end_tag(const string &tag_name) {
+        if (m_indent < 1)
+            throw runtime_error("Unbalanced tags in XML");
+        --m_indent;
+        indent();
+        m_out << "</" << tag_name << ">" << endl;
     }
-}
+};
 
 void convert_to_edrm(shared_ptr<pst> pst_file, ostream &loadfile,
                      const path &output_directory) {
-    using namespace xml;
-    size_t l = 0;
-    loadfile << xml_declaration()
-             << tag_open(l, "Root")
-             <<   attr("DataInterchangeType", L"Update")
-             << tag_close(l)
-             << start_tag(l, "Batch")
-             << start_tag(l, "Documents");
+    xml_context x(loadfile);
+
+    x.lt("Root").attr("DataInterchangeType", L"Update").gt();
+    x.start_tag("Batch");
+    x.start_tag("Documents");
 
     pst::message_iterator mi(pst_file->message_begin());
     for (; mi != pst_file->message_end(); ++mi) {
         document d(*mi);
-        loadfile << tag_open(l, "Document")
-                 <<   attr("DocType", L"Message") 
-                 << tag_close(l)
-                 << start_tag(l, "Tags");
+        x.lt("Document").attr("DocType", L"Message").gt();
+        x.start_tag("Tags");
         
         document::tag_iterator ti(d.tag_begin());
         for (; ti != d.tag_end(); ++ti) {
-            loadfile << tag_open(l, "Tag")
-                     <<   attr("TagName", ti->first)
-                     <<   attr("TagValue", edrm_tag_value(ti->second))
-                     <<   attr("TagDataType", edrm_tag_data_type(ti->second))
-                     << tag_close_empty(l);
+            x.lt("Tag")
+                .attr("TagName", ti->first)
+                .attr("TagValue", edrm_tag_value(ti->second))
+                .attr("TagDataType", edrm_tag_data_type(ti->second))
+                .slash_gt();
         }
 
-        loadfile << end_tag(l, "Tags")
-                 << end_tag(l, "Document");
+        x.end_tag("Tags");
+        x.end_tag("Document");
     }
 
-    loadfile << end_tag(l, "Documents")
-             << start_tag(l, "Relationships")
-             << end_tag(l, "Relationships")
-             << end_tag(l, "Batch")
-             << end_tag(l, "Root");
+    x.end_tag("Documents");
+    x.start_tag("Relationships");
+    x.end_tag("Relationships");
+    x.end_tag("Batch");
+    x.end_tag("Root");
 }

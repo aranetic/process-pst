700ed07fd8715d58e6ab2739d00a11cecb6e4ed8
Eric Kidd <git@randomhacks.net>
2010-07-01 10:38:05 -0400

    Add copyright notices to source files

--- xml_context.cpp
+++ xml_context.cpp
@@ -1,3 +1,22 @@
+// -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// process-pst: Convert PST files to RCF822 *.eml files and load files
+// Copyright (c) 2010 Aranetic LLC
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License (the "License")
+// as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but it
+// is provided on an "AS-IS" basis and WITHOUT ANY WARRANTY; without even
+// the implied warranties of MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, OR NONINFRINGEMENT.  See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <stdexcept>
 
 #include "utilities.h"
